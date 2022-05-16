61b08ff40c21d97a35a9a917beaffe1d102e58ec
Kevin Cubas <61201131+kevinCubas@users.noreply.github.com>
2022-05-16 01:12:12 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const allImages = document.querySelector('#allImages')
+
+let index = 0
+
+function displayImage() {
+  const images = allImages.querySelectorAll('img')
+  images.forEach(img => img.classList.remove('active-visibility'))
+  index = index + 1 === images.length ? 0 : index + 1
+  images[index].classList.add('active-visibility')
+}
+
+setInterval(() => displayImage(), 4000)
+console.log(allImages)
